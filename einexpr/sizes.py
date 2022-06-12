b0c507301038ae7643f3ec6c103b95d01cd3886e
IsaacBreen <57783927+IsaacBreen@users.noreply.github.com>
2022-06-12 19:29:42 +0800

    Some experiments

--- einexpr/sizes.py
+++ einexpr/sizes.py
@@ -1,13 +1,19 @@
 import numpy as np
+import numpy.typing as npt
+from numpy.typing import ArrayLike, DTypeLike, _ShapeLike
+import typing
+from typing import Any, Callable, Dict, List, Optional, Sequence, Tuple, Type, Union
+
 
 size_calcs = {}
 
-def register_out_shape(shaped_func):
+def register_out_shape(*shaped_funcs):
     """
     Register a function that precalculates shape of the output of a function.
     """
     def decorator(shape_precalculator):
-        size_calcs[shaped_func] = shape_precalculator
+        for shaped_func in shaped_funcs:
+            size_calcs[shaped_func] = shape_precalculator
         return shape_precalculator
     return decorator
 
@@ -22,8 +28,24 @@ def calculate_output_shape(func, *args, **kwargs):
         raise ValueError("No shape precalculator for function {}".format(func))
 
 
+class NewIndex:
+    pass
 
 
+@register_out_shape(np.sum)
+def numpy_sum_output_dims(a: ArrayLike, axis: Optional[Union[int, Sequence[int]]] = None, dtype: Optional[DTypeLike] = None, out: Optional[ArrayLike] = None, keepdims: Optional[bool] = None, initial: Optional[Any] = None, where: Optional[ArrayLike] = None) -> ArrayLike:
+    if axis is None:
+        return ()
+    if isinstance(axis, int):
+        return tuple(i for i in range(a.ndim) if i != axis)
+    else:
+        return tuple(i for i in range(a.ndim) if i not in axis)
 
-# @register_out_shape(np.sum)
-# def numpy_sum_out_shape(X,
+
+@register_out_shape(np.concatenate)
+def numpy_concatenate_output_dims(a: Sequence[ArrayLike], axis: Optional[int] = None, out: Optional[ArrayLike] = None, dtype: Optional[DTypeLike] = None, casting: Optional[str] = None) -> ArrayLike:
+    if axis is None:
+        return (NewIndex(),)
+    else:
+        return tuple(i if i != axis else NewIndex() for i in range(a[0].ndim))
+
--- tests/test_sizes.py
+++ tests/test_sizes.py
@@ -1,9 +1,16 @@
+import pytest
 import numpy as np
 from einexpr import calculate_output_shape
 from einexpr import einexpr
 
-
+@pytest.mark.skip
 def test_output_shape_numpy_sum():
-    X = einexpr(np.ones((2, 3)))['i j']
-    assert calculate_output_shape(np.sum, X) == ()
+    MAX_DIMS = 10
+    rng = np.random.default_rng()
+    X_ndim = rng.integers(1, MAX_DIMS)
+    X_shape = rng.random_integers(1, 10, size=(3))
+    print(X_shape)
+    
+    # X = einexpr(np.ones((2, 3)))['i j']
+    # assert calculate_output_shape(np.sum, X) == ()
     
