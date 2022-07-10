a6989271d98ee1814bfa66434eba39d940594818
IsaacBreen <57783927+IsaacBreen@users.noreply.github.com>
2022-07-10 20:33:29 +0800

    added dev_utils folder

---
+++ dev_utils/generate_array_api.py
@@ -0,0 +1,178 @@
+"""
+This script helps us to stay up-to-date with the latest Python Array API specification.  More specifically, it:
+1. downloads the specification from Github,
+2. checks that einexpr's implemention implements all of the API,
+3. adds the signature and docstring of any unimplemented functions (and sets the function body to raise a NotImplementedError),
+4. checks that the signatures of all implemented functions match those in the specification, and
+5. checks that the documentation of all implemented functions is a superset of the documentation in the specification (i.e. that, although it may add some documentation, it at least includes all information from the specification).
+"""
+import libcst as cst
+from libcst.tool import dump
+import libcst.matchers as m
+
+import os
+import sys
+import glob
+import shutil
+import pathlib
+import difflib
+
+from git import Repo
+
+import re
+
+
+def download_repo(repo_url, repo_subdir, save_dir, temp_dir="tmp"):
+    # Clone the folder array-api/spec/API_specification/array_api/ from https://github.com/data-apis/array-api/ into ./array_api
+    repo = Repo.init(temp_dir)
+
+    # Create a new remote if there isn't one already created
+    origin = repo.remotes[0] if repo.remotes else repo.create_remote("origin", repo_url)
+
+    origin.fetch()
+    git = repo.git()
+    git.checkout("origin/main", "--", repo_subdir)
+
+    # Move the repo to the save_dir, overwriting any existing files
+    copy_from = pathlib.Path(temp_dir) / repo_subdir
+    copy_to = pathlib.Path(save_dir)
+    if copy_to.exists():
+        shutil.rmtree(copy_to)
+    shutil.copytree(copy_from, copy_to)
+    
+    shutil.rmtree(temp_dir)
+
+
+class FunctionAndClassCollector(cst.CSTVisitor):
+    def __init__(self):
+        self.functions = {}
+        self.classes = {}
+
+    def visit_FunctionDef(self, node):
+        if node.name.value in self.functions:
+            raise Exception(f"Function {node.name.value} is defined twice")
+        self.functions[node.name.value] = node
+        return False
+    
+    def visit_ClassDef(self, node):
+        if node.name.value in self.classes:
+            raise Exception(f"Class {node.name.value} is defined twice")
+        self.classes[node.name.value] = node
+        return False
+
+
+def process_trees(einexpr_tree, template_tree):
+    # Collect all the functions and classes
+    einexpr_collector = FunctionAndClassCollector()
+    template_collector = FunctionAndClassCollector()
+    if isinstance(einexpr_tree, cst.ClassDef) and isinstance(template_tree, cst.ClassDef):
+        for child in einexpr_tree.body.body:
+            child.visit(einexpr_collector)
+        for child in template_tree.body.body:
+            child.visit(template_collector)
+    elif isinstance(einexpr_tree, cst.Module) and isinstance(template_tree, cst.Module):
+        einexpr_tree.visit(einexpr_collector)
+        template_tree.visit(template_collector)
+    
+    # Check that all functions are implemented with the correct signatures
+    for name in set(einexpr_collector.functions) & set(template_collector.functions):
+        einexpr_func = einexpr_collector.functions[name]
+        template_func = template_collector.functions[name]
+        if not (einexpr_func.params).deep_equals(template_func.params):
+            raise Exception(f"Parameters of function {name} do not match. Expected: {template_func.params}, got: {einexpr_func.params}")
+        if einexpr_func.returns != template_func.returns and einexpr_func.returns and not einexpr_func.returns.deep_equals(template_func.returns):
+            raise Exception(f"Returns of function {name} do not match. Expected: {template_func.returns}, got: {einexpr_func.returns}")
+    
+    # Insert the signature and docstring of any unimplemented functions and set the function body to raise a NotImplementedError
+    for name in set(template_collector.functions) - set(einexpr_collector.functions):
+        template_func = template_collector.functions[name]
+        template_func.body.body = list(template_func.body.body)
+        template_func.body.body.append(cst.parse_statement("raise NotImplementedError"))
+        einexpr_tree.body.append(template_func)
+        
+    # Insert any missing classes
+    for name in set(template_collector.classes) - set(einexpr_collector.classes):
+        template_class = template_collector.classes[name]
+        einexpr_tree.body.append(template_class)
+    
+    # Recurse over classes
+    for name in set(einexpr_collector.classes) & set(template_collector.classes):
+        einexpr_class = einexpr_collector.classes[name]
+        template_class = template_collector.classes[name]
+        process_trees(einexpr_class, template_class)
+        
+    return einexpr_tree
+
+class RaiseNotImplementedVisitor(m.MatcherDecoratableTransformer):
+    """
+    Add ``raise NotImplementedError`` to any function that has not been implemented.
+    """
+    @m.leave(
+        m.FunctionDef(
+            body=m.OneOf(
+                m.SimpleStatementSuite(
+                    body=[m.Expr(value=m.SimpleString())]
+                ),
+                m.IndentedBlock(
+                    body=[m.SimpleStatementLine([m.Expr(value=m.SimpleString())])]
+                )
+            )
+        )
+    )
+    def append_function_body_with_raise_NotImplementedError(self, original_node, updated_node):
+        return updated_node.with_changes(
+            body=updated_node.body.with_changes(
+                body=[*updated_node.body.body, cst.Expr(cst.parse_statement("raise NotImplementedError"))]
+            )
+        )
+
+def process_file(einexpr_file, template_file):    
+    # If the einexpr file doesn't exist, create it from the template file
+    if not os.path.exists(einexpr_file):
+        # Create the necessary (sub)directories
+        os.makedirs(os.path.dirname(einexpr_file), exist_ok=True)
+        with open(template_file, "r") as f, open(einexpr_file, "w") as g:
+            g.write(f.read())
+        with open(einexpr_file, "r") as f:
+            einexpr_tree = cst.parse_module(f.read())
+
+    # Load the einexpr file
+    einexpr_file_path = pathlib.Path(einexpr_file)
+    einexpr_file_path.parent.mkdir(parents=True, exist_ok=True)
+    einexpr_file_content = einexpr_file_path.read_text()
+
+    # Load the template file
+    template_file_path = pathlib.Path(template_file)
+    template_file_content = template_file_path.read_text()
+    
+    einexpr_tree = cst.parse_module(einexpr_file_content)
+    template_tree = cst.parse_module(template_file_content)
+
+    # Process the trees
+    einexpr_tree_new = process_trees(einexpr_tree, template_tree)
+    
+    # Raise ``NotImplementedError`` for any functions that are missing a body
+    visitor = RaiseNotImplementedVisitor()
+    einexpr_tree_new = einexpr_tree_new.visit(visitor)
+
+    # Save the einexpr file
+    einexpr_file_path.write_text(einexpr_tree_new.code)
+
+
+if __name__ == "__main__":
+    template_array_api_path = "einexpr/external_apis/array_api"
+    einexpr_array_api_path = "einexpr/array_api"
+
+    # Download the array API specification from Github
+    download_repo(
+        "https://github.com/data-apis/array-api/",
+        "spec/API_specification/array_api/",
+        template_array_api_path,
+        temp_dir="tmp_array_api"
+    )
+    
+    # Process the array API specification
+    for template_file in glob.glob(f"{template_array_api_path}/**/*.py", recursive=True):
+        einexpr_file = template_file.replace(template_array_api_path, einexpr_array_api_path)
+        print(f"Processing {template_file} -> {einexpr_file}")
+        process_file(einexpr_file, template_file)
---
+++ dev_utils/populate_array_api.py
@@ -0,0 +1,135 @@
+"""
+Implements the Array API in a semi-automated way.
+"""
+
+import libcst as cst
+from libcst.tool import dump
+import libcst.matchers as m
+from libcst.codemod.visitors import AddImportsVisitor
+from libcst.codemod import CodemodContext
+
+import os
+import sys
+import glob
+import shutil
+import pathlib
+import difflib
+
+from git import Repo
+
+import re
+
+
+single_arg_elementwise_function_names = "sign sqrt inv where clip argsort ones_like zeros_like full_like softmax log_softmax cumsum abs absolute sin cos tan exp log exp2 log2 log10 sqrt pow reciprocal floor ceil round floor_divide".split()
+multiple_arg_elementwise_function_names = []
+single_dimension_reduction_function_names = "sum product mean std max min maximum minimum all any".split()
+multiple_dimension_reduction_function_names = "argmax argmin".split()
+concatenation_function_names = "concat concatenate".split()
+
+
+class ImplementFunctions(m.MatcherDecoratableTransformer):
+    def __init__(self, *args, **kwargs):
+        self.implementation_helper_names_used = set()
+        super().__init__(*args, **kwargs)
+
+    @m.leave(
+        m.FunctionDef(
+            body=m.OneOf(
+                # TODO: This bit should be simplified and generalised to a function definition formatted any way, not just these two ways.
+                m.SimpleStatementSuite(
+                    body=[
+                        m.ZeroOrOne(m.Expr(value=m.SimpleString())),
+                        m.Raise(exc=m.Call(func=m.Name(value="NotImplementedError")))
+                    ]
+                ),
+                m.IndentedBlock(
+                    body=[
+                        m.ZeroOrOne(m.SimpleStatementLine(body=[m.Expr(value=m.SimpleString())])),
+                        m.SimpleStatementLine(
+                            body=[
+                                m.Raise(
+                                    exc=m.OneOf(
+                                        m.Name(value="NotImplementedError"),
+                                        m.Call(func=m.Name(value="NotImplementedError"),
+                                        )
+                                    )
+                                )
+                            ]
+                        )
+                    ]
+                )
+            )
+        )
+    )
+    def implement_function(self, original_node, updated_node):
+        # TODO: this assert will fail if the function is on a single line. Replace the Raise instead.
+        assert isinstance(raise_node := updated_node.body.body[-1].body[0], cst.Raise) and (raise_node.exc.value == "NotImplementedError" or raise_node.exc.func.value == "NotImplementedError"), f"Expected raise NotImplementedError, got {raise_node}"
+
+        func_name = original_node.name.value
+        if func_name in single_arg_elementwise_function_names:
+            implementation_helper_name = 'SingleArgumentElementwise'
+        elif func_name in multiple_arg_elementwise_function_names:
+            implementation_helper_name = 'MultiArgumentElementwise'
+        elif func_name in single_dimension_reduction_function_names:
+            implementation_helper_name = 'SingleDimensionReduction'
+        elif func_name in multiple_dimension_reduction_function_names:
+            implementation_helper_name = 'MultiDimensionReduction'
+        elif func_name in concatenation_function_names:
+            implementation_helper_name = 'Concatenation'
+        else:
+            return updated_node
+        
+        self.implementation_helper_names_used.add(implementation_helper_name)
+        
+        implementation_lines = [
+            f'out_dims = {implementation_helper_name}.calculate_output_dims(args, kwargs)',
+            f'ambiguous_dims = {implementation_helper_name}.calculate_output_ambiguous_dims(args, kwargs)',
+            f'processed_args, processed_kwargs = {implementation_helper_name}.process_args(args, kwargs)',
+            f'result = einexpr.einarray({func_name}(*processed_args, **processed_kwargs), dims=out_dims, ambiguous_dims=ambiguous_dims)',
+            f'return result',
+        ]
+        
+        implementation_expressions = [cst.parse_statement(line) for line in implementation_lines]
+
+        print(f"    Implementing {func_name} with {implementation_helper_name}")
+
+        return updated_node.with_changes(
+            body=updated_node.body.with_changes(
+                body=cst.FlattenSentinel([
+                    *updated_node.body.body[:-1],
+                    *implementation_expressions
+                ])
+            )
+        )
+
+
+def process_file(file_path):
+    """
+    For each function definition in the file, set the function body to the corresponding 
+    """
+    # Load the file as a CST tree
+    with open(file_path, 'r') as f:
+        code = f.read()
+    tree = cst.parse_module(code)
+    # Implement the functions
+    implement_functions_transformer = ImplementFunctions()
+    tree = tree.visit(implement_functions_transformer)
+    # Import the necessary helpers
+    codemod_context = CodemodContext()
+    add_imports_visitor = AddImportsVisitor(codemod_context)
+    add_imports_visitor.add_needed_import(codemod_context, 'einexpr', 'einarray')
+    for helper_name in implement_functions_transformer.implementation_helper_names_used:
+        add_imports_visitor.add_needed_import(codemod_context, 'einexpr', helper_name)
+    tree = add_imports_visitor.transform_module(tree)
+    # Write the code back to the file
+    with open(file_path, 'w') as f:
+        f.write(tree.code)
+
+
+if __name__ == '__main__':
+    einexpr_array_api_path = "einexpr/array_api"
+    
+    # Get the list of files to process
+    for file_path in glob.glob(f"{einexpr_array_api_path}/**/*.py", recursive=True):
+        print(f"Processing {file_path}")
+        process_file(file_path)
