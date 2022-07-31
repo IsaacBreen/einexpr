e1999a1bedb7a440253e95d42bb4de1855147dbd
IsaacBreen <57783927+IsaacBreen@users.noreply.github.com>
2022-07-31 16:03:19 +0800

    add dtypes

--- einexpr/array_api/__init__.py
+++ einexpr/array_api/__init__.py
@@ -3,7 +3,7 @@ from .dimension import *
 from .constants import *
 from .creation_functions import *
 from .data_type_functions import *
-from . import data_types as dtype
+from .data_types import *
 from .elementwise_functions import *
 from .linear_algebra_functions import *
 from .manipulation_functions import *
